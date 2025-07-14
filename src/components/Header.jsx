948ffa8cb06785ee3233ac70a9e336dc87563272
Davi Matheus <davimateusabreuw@gmail.com>
2025-07-14 19:56:50 -0300

    modo escuro

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
   return (
-    <header className="m-10 w-full min-h-full from-stone-900 py-8 shadow-lg">
-      <div className="max-w-6xl mx-auto px-6 flex flex-col items-center text-center">
+    <header className="mb-10 w-full min-h-full bg-stone-800 shadow-lg">
+      <div className="my-10 max-w-6xl mx-auto px-6 flex flex-col items-center text-center">
         <h1 className="text-4xl md:text-5xl font-extrabold text-white drop-shadow-lg">
-          MARK MOVIES
+          INFOMOVIES
         </h1>
         <p className="mt-4 text-lg md:text-xl text-gray-300">
           Explore os filmes mais populares do momento
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 
 function Home() {
   return (
-    <div className="w-full min-h-screen overflow-x-hidden flex flex-col justify-start items-center p-6 gap-5">
+    <div className="w-full min-h-screen overflow-x-hidden flex flex-col justify-start items-center p-6 gap-5 bg-stone-900 text-gray-300">
       <Header />
       <ListMovies />
     </div>
